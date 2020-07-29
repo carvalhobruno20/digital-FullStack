a64634a439e07c8d7a2c8dd76240bbb9dc4d0acc
Bruno Teixeira de Carvalho <carvalho.bruno20@gmail.com>
2020-07-28 21:08:57 -0300

    Novo Repositorio

---
+++ Aula07/js/script.js
@@ -0,0 +1,12 @@
+var positivo = 5;
+var negativo = -8;
+var decimal = 1.55;
+//var nome = "Bruno";
+//var sobrenome = "Carvalho";
+//var nome = Clara;
+var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris elementum suscipit bibendum. Quisque sed risus viverra, pretium massa et, mollis turpis. Aenean consectetur turpis justo, vitae euismod quam dictum quis. Sed sed ullamcorper odio. Donec eget suscipit ex, a iaculis urna."
+var valorNulo = null;
+var naoDefinido = undefined;
+var array = ['pera','uva','banana','abacate','laranja'];
+var string = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam";
+console.log(string)
